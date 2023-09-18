7b769e1fdd5421a7cd024bd52d962c0cf15336e1
thomazac <thomazac@oregonstate.edu>
2023-09-17 18:19:29 -0700

    Created empty storage files for json data, fixes to python data grabbers, added moxa-pocs compatible logging

--- packages/panoptes3d/system_integration/get_current_target.py
+++ packages/panoptes3d/system_integration/get_current_target.py
@@ -10,21 +10,27 @@ from observational_scheduler.obs_scheduler import target
 from logger.astro_logger import astroLogger
 
 def main():
-
+    logger = astroLogger(enable_color=True)
+    logger.info("Fetching current target and system state...")
+    logger.debug("Accessing current_target.pickle")
     with open(f"{PARENT_DIRECTORY}/pickle/current_target.pickle", "rb") as file:
         current_target = pickle.load(file)
+    logger.debug("Done")
 
-    with open(f"{PARENT_DIRECTORY}/pickle/system_info.pickle") as file:
+    logger.debug("Accessing system_info.pickle")
+    with open(f"{PARENT_DIRECTORY}/pickle/system_info.pickle", "rb") as file:
         system_info = pickle.load(file)
+    logger.debug("Done")
+    logger.info("Fetch complete.")
 
-    current_target = current_target.__dict__()
+    current_target = current_target.__dict__
     
     allData = {**current_target, **system_info}
-    print(allData)
     allDataJSON = json.dumps(allData)
 
-    with open(f"{__file__}/json_data/target.json", "w") as file:
+    with open(f"{os.path.dirname(__file__)}/json_data/target.json", "w") as file:
         file.write(allDataJSON)
+    logger.info(f"Wrote json data to {os.path.dirname(__file__)}/json_data/target.json")
 
 if __name__ == "__main__":
     main()
--- packages/panoptes3d/system_integration/get_moxa_pocs_settings.py
+++ packages/panoptes3d/system_integration/get_moxa_pocs_settings.py
@@ -1,22 +1,31 @@
 import os
+import sys
 import json
 
 from yaml import safe_load
+
+PARENT_DIRECTORY = os.path.dirname(__file__).replace("/packages/panoptes3d/system_integration", "")
+sys.path.append(PARENT_DIRECTORY)
+
+from logger.astro_logger import astroLogger
 
 '''
 When run on a system with moxa-pocs, this will find static system information and put it into system_info.json
 '''
 def main():
-    # Static information is stored in settings.yaml
-    PARENT_DIRECTORY = os.path.dirname(__file__).replace("/packages/panoptes3d/system_integration", "")
+    logger = astroLogger(enable_color=True)
+    logger.info("Fetching current system settings...")
+    logger.debug("Accessing settings.yaml")
     with open(f"{PARENT_DIRECTORY}/conf_files/settings.yaml", "r") as file:
         settings = safe_load(file)
+    logger.debug("Done")
+    logger.info("Fetch complete")
     
     settingsJSON = json.dumps(settings)
     
-    with open(f"{__file__}/json_data/settings.json", "w") as file:
+    with open(f"{os.path.dirname(__file__)}/json_data/settings.json", "w") as file:
         file.write(settingsJSON)
+    logger.info(f"Wrote json data to {os.path.dirname(__file__)}/json_data/settings.json")
 
 if __name__ == "__main__":
-    print(__file__)
     main()
